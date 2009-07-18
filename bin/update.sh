9ab1d09f5042cadfab6653a876adb82e0c6bf795
Liam Cooke <liamcooke@gmail.com>
2009-07-18 01:56:36 +0100

    minify javascript before uploading

--- bin/update.sh
+++ bin/update.sh
@@ -7,8 +7,11 @@ outputdir=../html
 # generate button image if it doesn't exist
 ([ -e $outputdir/butan-original.png ] && [ -e $outputdir/icon.png ]) || ./make_butan.sh
 
+# minify javascript
+cp ../src/jquery-1.3.2.min.js $outputdir/pushbutan.js
+cat ../src/{plugins,pushbutan}.js | ./jsmin.py >> $outputdir/pushbutan.js
+
 # copy source files to the output directory
-cat ../src/{jquery-1.3.2.min,plugins,pushbutan}.js > $outputdir/pushbutan.js
 cp -p \
     ../src/.htaccess \
     ../src/*.{py,cgi,html,txt,css} \
