80acc8dbce7412ec730cde6d9199e81a1a24fd2b
Liam Cooke <liamcooke@gmail.com>
2010-03-04 10:36:35 +0000

    new blog URL

--- src/data.py
+++ src/data.py
@@ -5,7 +5,7 @@ __author__ = 'Liam Cooke <http://boxofjunk.ws/>'
 footer = []
 footer.append('<p>%s</p>' % '\n&middot; '.join((
     '<a href="#" id="wut">wut?</a>',
-    '<a href="http://automeme.tumblr.com/">Blog</a>',
+    '<a href="http://blog.automeme.net/">Blog</a>',
     '<a href="/api.html">API</a>',
     '<a href="/stuff.html">Moar</a>',
     )) )
