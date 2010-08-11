5827d091e016dd788152065dfc1c4a3030868800
Liam Cooke <liamcooke@gmail.com>
2010-08-11 23:43:00 +0100

    Z҉A҉L҉G҉O̚̕̚ Z҉A҉L҉G҉O̚̕̚ Z҉A҉L҉G҉O̚̕̚

--- src/pushbutan.js
+++ src/pushbutan.js
@@ -1,11 +1,118 @@
 //if (top !== self)
 //    top.location.href = self.location.href;
 
+function Zalgo()
+{
+    // ----- modified from http://github.com/Marak/zalgo.js -----
+    var up = [
+    '̍','̎','̄','̅',
+    '̿','̑','̆','̐',
+    '͒','͗','͑','̇',
+    '̈','̊','͂','̓',
+    '̈','͊','͋','͌',
+    '̃','̂','̌','͐',
+    '̀','́','̋','̏',
+    '̒','̓','̔','̽',
+    '̉','ͣ','ͤ','ͥ',
+    'ͦ','ͧ','ͨ','ͩ',
+    'ͪ','ͫ','ͬ','ͭ',
+    'ͮ','ͯ','̾','͛',
+    '͆','̚'
+    ];
+
+    var down = [
+          '̖','̗','̘','̙',
+          '̜','̝','̞','̟',
+          '̠','̤','̥','̦',
+          '̩','̪','̫','̬',
+          '̭','̮','̯','̰',
+          '̱','̲','̳','̹',
+          '̺','̻','̼','ͅ',
+          '͇','͈','͉','͍',
+          '͎','͓','͔','͕',
+          '͖','͙','͚','̣'];
+
+    var mid = [
+          '̕','̛','̀','́',
+          '͘','̡','̢','̧',
+          '̨','̴','̵','̶',
+          '͜','͝','͞',
+          '͟','͠','͢','̸',
+          '̷','͡',' ҉'];
+
+    var all = [].concat(up,down,mid);
+
+    var zalgo = {};
+
+    function randomNumber(range) {
+      r = Math.floor(Math.random()*range);
+      return r;
+    };
+
+    function is_char(character) {
+      var bool = false;
+      all.filter(function(i){
+       bool = (i == character);
+      })
+      return bool;
+    }
+
+    zalgo.heComes = function(text, u, m, d, size){
+        result = '';
+
+        var options = {"up" : u || true, "down" :  d || true, "mid" : m || true, "size" : size || "mini"};
+
+        text = text.split('');
+         for(var l in text){
+           if(is_char(l)) {
+             continue;
+           }
+           result = result + text[l];
+
+           var counts = {"up" : 0, "down" : 0, "mid" : 0};
+
+          switch(options.size) {
+            case 'mini':
+              counts.up = randomNumber(8);
+              counts.min= randomNumber(2);
+              counts.down = randomNumber(8);
+            break;
+            case 'maxi':
+              counts.up = randomNumber(16) + 3;
+              counts.min = randomNumber(4) + 1;
+              counts.down = randomNumber(64) + 3;
+            break;
+            default:
+              counts.up = randomNumber(8) + 1;
+              counts.mid = randomNumber(6) / 2;
+              counts.down= randomNumber(8) + 1;
+            break;
+          }
+          var arr = ["up", "mid", "down"];
+          for(var d in arr){
+            var index = arr[d]
+            for (var i = 0 ; i <= counts[index]; i++)
+            {
+              if(options[index]) {
+                  p = eval(index)
+                  result = result + p[randomNumber(p.length)]
+                }
+              }
+            }
+          }
+        return result;
+    };
+    // ----- end of zalgo.js ----- */
+
+    return zalgo.heComes;
+}
+
 function AutoMeme(tagButton, tagMeme)
 {
-    var memes = new Array();
-    var blocked = false;
-    var max = 10;
+    var memes = new Array(),
+        blocked = false,
+        max = 10,
+        zalgo = null;
 
     unblock();
 
@@ -44,8 +151,11 @@ function AutoMeme(tagButton, tagMeme)
 
     function pop()
     {
-        if (memes.length > 0)
-            tagMeme.innerHTML = memes.shift();
+        if (memes.length > 0) {
+            var m = memes.shift();
+            if (zalgo) m = zalgo(m);
+            tagMeme.innerHTML = m;
+        }
     }
 
     function fetch(callback)
@@ -96,7 +206,17 @@ function AutoMeme(tagButton, tagMeme)
     $('body').upUpDownDown({
         watchFor: [38,38,40,40,37,39,37,39,66,65],
         callback: function() {
-            $('body').html('<center><img src="SiteSeizedNOTICE.jpg"/></center>');
+            if (!zalgo) {
+                zalgo = Zalgo();
+                $('body').css({
+                    fontFamily: "Helvetica, Arial, 'DejaVu Sans', sans-serif",
+                    overflowX: 'hidden'
+                });
+                tagMeme.innerHTML = zalgo(tagMeme.innerHTML);
+                $('#footer-links a, #halp h1, #halp a').each(function() {
+                    $(this).html(zalgo($(this).html()));
+                });
+            }
         }
     });
 
